e99f2c835c9461e338b8d923b2fd24d2e1730887
batlicka <vojta@vagunda.eu>
2021-03-02 21:37:41 +0100

    serialization from JSON file

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,7 +1,9 @@
+import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.exc.UnrecognizedPropertyException;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import org.apache.http.HttpEntity;
 
 import org.apache.http.client.methods.*;
@@ -15,6 +17,7 @@ import org.apache.http.impl.client.HttpClients;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 
 public class MainClass {
@@ -28,16 +31,32 @@ public class MainClass {
 
     public static void main(String[] args) throws IOException {
             //přidej výjimku, co když odpověď nedojde ve fromátu JSON
+            System.out.println("Working Directory = " + System.getProperty("user.dir"));
 
+            ObjectMapper objectMapper = new ObjectMapper();
+
+            File file = new File("RuleViolationException.json");
+            JsonNode rootNode2 = objectMapper.readTree(file);
+
+            ArrayList<String> arr= new ArrayList<String>();
+
+            ArrayNode arrayNode = (ArrayNode) rootNode2.at("/RuleViolationExceptions");
+
+            for (int i = 0; i < arrayNode.size(); i++) {
+                arr.add(arrayNode.get(i).asText());
+            }
+            ///////////////////////////
             String filePath="D:\\dokumenty\\Vojta\\UTB\\diplom_prace\\profiles\\veraPDF-corpus\\PDF_A-1b\\6.1 File structure\\6.1.2 File header\\veraPDF test suite 6-1-2-t01-pass-a.pdf";
             String fileName="veraPDF test suite 6-1-2-t01-pass-a.pdf";
 
             CloseableHttpClient client = HttpClients.createDefault();
-            HttpPost httpPost = new HttpPost("http://pdfa.k.utb.cz:8080/api/validate/auto");
+            HttpPost httpPost = new HttpPost("http://pdfa.k.utb.cz:8080/api/validate/auto");//http://pdfa.k.utb.cz:8080/api/validate/auto
+            //http://localhost:8080/api/validate/auto
+            //http://pdfa.k.utb.cz:8080/api/validate/auto
 
             MultipartEntityBuilder builder = MultipartEntityBuilder.create();
-            builder.addBinaryBody(
-                    "file", new File(filePath), ContentType.APPLICATION_OCTET_STREAM, fileName);//
+            //builder.addBinaryBody("file", new File(filePath), ContentType.APPLICATION_OCTET_STREAM, fileName);
+            builder.addBinaryBody("file",  new File(filePath));
             HttpEntity multipart = builder.build();
             httpPost.setEntity(multipart);
 
@@ -61,6 +80,8 @@ public class MainClass {
                         des.getAttributeValueFromRoot("pdfaflavour"),
                         des.getClauseArray()
                 );
+                //rest api rozhodne, jak se výjimka ošetří
+                //na zobrazování chyb použít běžné http kody a chybu specifikovat v jeho správě
 
                 System.out.println("|Compliant: " + responseCurrent.getCompliant() +"|pdfaflavour: "+responseCurrent.getPdfaflavour());
                 System.out.println("List of Clauses: " + responseCurrent.getListRuleViolationClause());
@@ -68,6 +89,8 @@ public class MainClass {
                 System.out.println(e1.getMessage());
             }catch(JsonMappingException e2){
                 System.out.println(e2.getMessage());
+            }catch (JsonParseException e3){
+                System.out.println("response can't be processed, because response is not in JSON format");
             }
 
 
